13da695f65f7ac50b97b82ccda9b052faefbeba0
msemenova <semenovamaria19981@gmail.com>
2022-05-25 11:11:06 +0300

    Changed a couple of lines

--- test2cookies.py
+++ test2cookies.py
@@ -3,7 +3,10 @@ class Test2Cookies:
     def test_cookies(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
         print(response.text)
+        print(response.status_code)
         print(dict(response.cookies))
         name = response.text
-        assert name == response.text
+        a = response.status_code
+        assert name == response.text and a == 200
+
 
--- test_headers.py
+++ test_headers.py
@@ -4,7 +4,7 @@ class Test2Cookies:
         response1 = requests.post("https://playground.learnqa.ru/api/homework_header")
         print(response1.headers)
         print(response1.status_code)
-        a =response1.headers
+        a = response1.headers
         b = response1.status_code
         assert b == 200 and a == response1.headers
 
